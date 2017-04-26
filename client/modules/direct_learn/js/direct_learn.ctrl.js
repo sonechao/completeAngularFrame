3e98abe21c0ca43c18612a2fde23f3a3409c785e
songchao <sconghao@163.com>
2017-04-26 15:23:25 +0800

    modified directive info

--- client/modules/direct_learn/js/direct_learn.ctrl.js
+++ client/modules/direct_learn/js/direct_learn.ctrl.js
@@ -10,8 +10,8 @@
 		var vm = this;
 		console.log('这里是指令的控制器！。。。。。。。。。。。。。');
 
-		vm.legend = ["legend1", "legend2",'legend3','legend4'];  
-	    vm.item = ['星期一', '星期2', '星期3', '星期4', '星期5', '星期6', '星期7', '星期8', '星期9', '星期10', '星期11', '星期12'];  
+		vm.legend = ["Berlin", "London",'New York','Tokyo'];  
+	    vm.item = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];  
 	    vm.data = [  
 	        [-1, 1, 3, 7, 13, 16, 18, 16, 15, 9, 4, 2], //Berlin  
 	        [0, 1, 4, 7, 12, 15, 16, 15, 15, 10, 6, 5], //London  
